f499add137c95d2d0f83831ba902edd02da3cc33
Timothy Teoh <timothy.teoh@frogasia.com>
2018-03-19 22:00:18 +0800

    Added components to display pricing rules

--- resources/assets/js/components/CustomerPricingRule.js
+++ resources/assets/js/components/CustomerPricingRule.js
@@ -1,20 +1,21 @@
 const React = require('react')
 const api = require('../utils/api')
-import { Button, Table } from 'react-bootstrap'
+import { Breadcrumb, Button, Col, FormControl, Grid, Panel, Row } from 'react-bootstrap'
 
 export default class CustomerPricingRule extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      rules: null
+      rule: null
     }
   }
 
   componentDidMount() {
     api.getCustomerPricingRule(this.props.match.params.ruleId)
-    .then(({ }) => {
+    .then((rule) => {
+      console.log(rule)
       this.setState({
-        rules
+        rule
       })
     })
   }
@@ -22,7 +23,37 @@ export default class CustomerPricingRule extends React.Component {
   render() {
     return (
       <div>
-
+        <Breadcrumb>
+          <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+          <Breadcrumb.Item active>Rule: {this.state.rule && this.state.rule.display_name}</Breadcrumb.Item>
+        </Breadcrumb>;
+        {this.state.rule &&
+        <Panel>
+          <h1>{this.state.rule.display_name}</h1>
+          <Grid>
+            <Row>
+              <Col md={3} xs={3}>
+                <strong>Base Rule:</strong>
+              </Col>
+              <Col md={3} xs={3}>
+                <p>{this.state.rule.pricingRule.display_name}</p>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={3} xs={3}>
+                <strong>Settings:</strong>
+              </Col>
+              <Col md={3} xs={3}>
+                <ul>
+                  {Object.keys(this.state.rule.pricing_rule_settings).map((key, i) => (
+                    <li key={i}>{key}: {this.state.rule.pricing_rule_settings[key]}</li>
+                  ))}
+                </ul>
+              </Col>
+            </Row>
+          </Grid>
+        </Panel>
+      }
       </div>
     )
   }
--- resources/assets/js/components/CustomerPricingRules.js
+++ resources/assets/js/components/CustomerPricingRules.js
@@ -21,15 +21,29 @@ export default class CustomerPricingRules extends React.Component {
 
   render() {
     return (
-      <Table>
-        {this.state.rules && this.state.rules.map((rule, i) => (
-          <tr>
-            <td>{rule.customer.name}</td>
-            <td>{rule.display_name}</td>
-            <td><Button href={`/customer-pricing-rule/${rule.id}`}>Edit</Button></td>
-          </tr>
-        ))}
-      </Table>
+      <div>
+        <h1>Customer Pricing Rules</h1>
+        <Table>
+          <thead>
+            <tr>
+              <th>Customer</th>
+              <th>Rule</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+          {this.state.rules && this.state.rules.map((rule, i) => (
+            <tr key={i}>
+              <td>{rule.customer.name}</td>
+              <td>{rule.display_name}</td>
+              <td>
+                <Button bsStyle='info' href={`/customer-pricing-rule/${rule.id}`}>Edit</Button>
+              </td>
+            </tr>
+          ))}
+          </tbody>
+        </Table>
+      </div>
     )
   }
 }
